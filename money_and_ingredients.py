9170d26cadfcd5c9505cbe2bbda9b098cd3e4650
NateThePigeonTree <nd536857@georgetownisd.org>
2026-01-22 19:22:28 -0600

    Add files via upload
    my first attempt on this, VS code's terminal was not working for me

---
+++ money_and_ingredients.py
@@ -0,0 +1,9 @@
+def money_and_ingredients():
+    money=200
+    sugar=1
+    lemons=.50
+    ice=2.50
+    cups=2.50
+    amount_of_lemons=input("how many lemons would you like to buy?")
+    for i in range(amount_of_lemons):
+        money-lemons
