9b2118b3c3089a193a4284e12a7c1aba2b5e8e18
NateThePigeonTree <nd536857@georgetownisd.org>
2026-01-28 15:26:23 -0600

    Update tracking.py

--- tracking.py
+++ tracking.py
@@ -2,29 +2,50 @@ lemons = -5
 sugar = -1
 ice= -3
 cups=-3
-def tracking(cash, value):
-    cash += value
-    return cash
+p_sugar = 0
+p_lemons = 0
+p_ice = 0
+p_cups = 0
+cash = 100
 
-def shop(cash):
+
+def tracking(value, modifier):
+    value += modifier
+    return value
+
+def shop():
+    global cash
+    global p_lemons 
+    global p_sugar
+    global p_ice
+    global p_cups
+    #p_ice, p_cups, p_sugar
     whichProduct= input("Which product do you want to buy? Lemons/Sugar/Ice/Cups\n")
-    if whichProduct.lower=="lemons":
+    if whichProduct.lower()=="lemons":
         cash = tracking(cash,lemons)
-        return(cash)
-    if whichProduct.lower()=="sugar":
+        p_lemons = tracking(p_lemons,50)
+        return
+    elif whichProduct.lower()=="sugar":
         cash = tracking(cash,sugar)
-        return(cash)
-    if whichProduct.lower()=="ice":
+        p_sugar = tracking(p_sugar,25)
+        return
+    elif whichProduct.lower()=="ice":
         cash = tracking(cash,ice)
-        return(cash)
-    if whichProduct.lower()=="cups":
-        cash = tracking(cash,cups)
-        return(cash)
-# shop
+        p_ice = tracking(p_ice,25)
+        return
+    elif whichProduct.lower()=="cups":
+        cash== tracking(cash,cups)
+        p_cups = tracking(p_cups,100)
+        return
+    else:
+        return 0
 
-
-
-print(shop(100))
     # BUY LEMONS
+
     # LEMONS ARE 5$
     # Money = tracking(Money, lemon_5)
+
+while True:
+    if shop() == 0:
+        break
+print(f"Cash: {cash}\nIce: {p_ice}\nLemons: {p_lemons}\nSugar: {p_sugar}\nCups: {p_cups}")
