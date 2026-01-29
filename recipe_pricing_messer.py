074a331719a16b70acf3a27122fa2f18a35851ea
brooksmesser <bm536931@georgetownisd.org>
2026-01-29 15:13:57 -0600

    Update recipe_pricing_messer.py

--- recipe_pricing_messer.py
+++ recipe_pricing_messer.py
@@ -1,5 +1,2 @@
-recipe={"Lemons":int(input("How many lemons do you want 1/2/3:")),
-        "Sugar":int(input("How much sugar do you want 1/2/3:")),
-        "Ice":int(input("How many ice cubes do you want 1/2/3:"))
-        } 
-price=int(input("How much would you like to make your lemonade cost:"))
+
+
