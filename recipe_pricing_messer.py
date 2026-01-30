3d5a4daaf26f415772649b8fd6ccfea97214828d
brooksmesser <bm536931@georgetownisd.org>
2026-01-29 22:24:40 -0600

    Update recipe_pricing_messer.py

--- recipe_pricing_messer.py
+++ recipe_pricing_messer.py
@@ -1,8 +1,16 @@
-inventory={"Lemons":0,
-        "Sugar":0,
-        "ice":0,
-         "cups":0
-          }
+def inventory():
+        inven={"Lemons":30
+                "Sugar":50,
+                "ice":50
+                }
+
+def recipe():
+        lemons=int(input("How many lemons would you like (ex.1/2/3):"))
+        sugar=int(input("How much sugar would you like (ex.1/2/3):"))
+        ice=int(input("How much ice would you like (ex.1/2/3):"))
+
+        
+
 
 
 
