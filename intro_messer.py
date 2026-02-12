c23fa19f24906cac41356b3a2c7f265d29b1047d
NathanielJohns <nj557770@georgetownisd.org>
2026-02-12 14:54:02 -0600

    story line update

--- intro_messer.py
+++ intro_messer.py
@@ -1,4 +1,4 @@
 #intro to game like storylin# 
 def introduction():
     name=input("Hello!\n What is your name:")
-    print("Well",name,"you have just broken your living room TV while your parents are on a 7 day vacation away from home.\nSo now you have decided to make a lemonade stand to raise the $300 you need to buy a new TV.\nGOODLUCK AND HAVE FUN!")
+    print("Well",name,"you were watching football and your team lost oh no you crashed out and the living room TV while your parents are on a 7 day vacation away from home.\nSo now you have decided to make a lemonade stand to raise the $300 you need to buy a new TV.\nGOODLUCK AND HAVE FUN!")
