1f988c2e57ad9481b92f571d63cb1690f70fe45e
NateThePigeonTree <nd536857@georgetownisd.org>
2026-02-19 15:27:05 -0600

    changed intro

--- intro_messer.py
+++ intro_messer.py
@@ -1,3 +1,5 @@
 #intro to game like storylin# 
 name=input("Hello!\nWhat is your name:")
-print("Well",name,"you have just broken your living room TV while your parents are on a 7 day vacation away from home.\nSo now you have decided to make a lemonade stand to raise the $300 you need to buy a new TV.\nGOODLUCK AND HAVE FUN!\n==================")
+print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~BACKGROUND~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+check = input("It is november 28th 2025(press enter to continue)")
+check = input("You are watching the Texas V.S Texas A&M game")
