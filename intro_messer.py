2ecc833097c40dc94b2ddc003c8740d01a55cfde
NathanielJohns <nj557770@georgetownisd.org>
2026-03-04 15:20:47 -0600

    Update intro_messer.py

--- intro_messer.py
+++ intro_messer.py
@@ -1,4 +1,4 @@
 #intro to game like storylin# 
 def intro(name):
-    print(f"Well {name} it is summertime and your friend's birthday is coming up,\n and your friend wants some new nintendo switch 2 games. However because the switch 2 cost your life savings plus the average mortage,\n you decide to open a lemonade stand and see if you can make enough to buy one.\n You have seven days til his birthday, and you have to make 100 dollars.")
+    print(f"Well {name} it is summertime and your friend's birthday is coming up,\n and your friend wants some new nintendo switch 2 games. However because the switch 2 cost your life savings plus the average mortage,\n you decide to open a lemonade stand and see if you can make enough to buy one.\n You have seven days til his birthday, and you have to make 200 dollars.")
     print("~~~~~~~~~~~~~~~~~~~~~~GOOD LUCK!~~~~~~~~~~~~~~~~~~~~~~~~~")
